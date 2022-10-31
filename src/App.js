4b6f760ecd8ecdc71f2ffd134f1fef9d804f7939
mercysolomon <merciisolomon@gmail.com>
2022-10-31 18:39:48 +0100

    removed links component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from "react";
 import "./App.css";
+import Profile from "./components/Profile.js";
 
 function App() {
- 
+  return (
+    <div>
+      <Profile picture="./pic.jpg" name="Mercy Solomon" />
 
-  return ()
+      <a id="twitter" href="https://www.twitter.com/etima_">
+        Twitter Link
+      </a>
+
+      <a id="btn__zuri" href="https://training.zuri.team/">
+        Zuri Team
+      </a>
+
+      <a id="books" href="https://books.zuri.team/">
+        Zuri Books
+      </a>
+
+      <a id="books__python" href="https://boks.zuri.team/">
+        Python Books
+      </a>
+
+      <a id="pitch" href="https://background.zuri.team/">
+        Background Check For Coders
+      </a>
+
+      <a id="book__design" href="https://training.zuri.team/design-rules">
+        Design Books
+      </a>
+    </div>
+  );
 }
 
 export default App;
